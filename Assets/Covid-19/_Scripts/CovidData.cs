42dd916843c28ffaa0ddf1c6608ab88ab854d476
amit07 <amitghimiray07@outlook.com>
2020-09-27 09:19:18 +0545

    Added Project Files

---
+++ Assets/Covid-19/_Scripts/CovidData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CovidData : MonoBehaviour, IPointerDownHandler
+{
+    public string place;
+    public int confirmed;
+    public int recovered;
+    public int fatal;
+
+    public GameObject SelectedInfoPanel;
+    public TextMeshProUGUI LocationUI;
+    public TextMeshProUGUI ConfirmedUI;
+    public TextMeshProUGUI RecoveredUI;
+    public TextMeshProUGUI FatalUI;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        SelectedInfoPanel = GameObject.FindGameObjectWithTag("SelectedInfoPanel");
+        LocationUI = SelectedInfoPanel.transform.Find("Location").GetComponent<TextMeshProUGUI>();
+        ConfirmedUI = SelectedInfoPanel.transform.Find("Confirmed").GetComponent<TextMeshProUGUI>();
+        RecoveredUI = SelectedInfoPanel.transform.Find("Recovered").GetComponent<TextMeshProUGUI>();
+        FatalUI = SelectedInfoPanel.transform.Find("Fatal").GetComponent<TextMeshProUGUI>();
+        SelectedInfoPanel.SetActive(true);
+        LocationUI.text = place;
+        ConfirmedUI.text = "Confirmed: " + confirmed.ToString();
+        RecoveredUI.text = "Recovered: " + recovered.ToString();
+        FatalUI.text = "Fatal: " + fatal.ToString();
+    }
+}
---
+++ Assets/Covid-19/_Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject InfoPanel;
+    public Toggle InfoPanelToggle;
+    public GameObject SelectedInfoPanel;
+    public Toggle SelectedInfoPanelToggle;
+    public void ToggleInfoPanel() 
+    {
+        InfoPanel.SetActive(InfoPanelToggle.isOn);
+    }
+
+    public void ToggleSelectedInfoPanel() 
+    {
+        SelectedInfoPanel.SetActive(SelectedInfoPanelToggle.isOn);
+    }
+}
